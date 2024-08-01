eeb30c198cb73b35fe47c7cb1e2459b86dd905b7
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-08-01 14:13:41 +0700

    update lib and poc auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: vercel(),
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), auth()],
+  output: 'server',
+  adapter: vercel()
 });
