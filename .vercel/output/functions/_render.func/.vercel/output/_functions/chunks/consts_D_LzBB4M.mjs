1b283f8129ca46c9f596fb24a60a80dca2f11e75
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-08-07 13:42:26 +0700

    del

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/consts_D_LzBB4M.mjs
+++
@@ -1,4 +0,0 @@
-const SITE_TITLE = "Restaurant Reservation";
-const SITE_DESCRIPTION = "Welcome to Restaurant";
-
-export { SITE_TITLE as S, SITE_DESCRIPTION as a };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/api/calculate.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/calculate_ChG6-Qjo.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
